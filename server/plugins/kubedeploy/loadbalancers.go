12d40fc4f8c377d26fa2e9cd8231f0660f222a65
Jeremy Deininger <jeremydeininger@gmail.com>
2017-01-17 10:09:06 -0800

    adding additional type for TCP/SSL lb

--- server/plugins/kubedeploy/loadbalancers.go
+++ server/plugins/kubedeploy/loadbalancers.go
@@ -106,12 +106,10 @@ func (x *KubeDeploy) doLoadBalancer(e agent.Event) error {
 		serviceType = v1.ServiceTypeClusterIP
 	case plugins.External:
 		serviceType = v1.ServiceTypeLoadBalancer
-		serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-proxy-protocol"] = "*"
 		serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-connection-draining-enabled"] = "true"
 		serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-connection-draining-timeout"] = "300"
 	case plugins.Office:
 		serviceType = v1.ServiceTypeLoadBalancer
-		serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-proxy-protocol"] = "*"
 		serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-internal"] = "0.0.0.0/0"
 		serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-connection-draining-enabled"] = "true"
 		serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-connection-draining-timeout"] = "300"
@@ -122,9 +120,14 @@ func (x *KubeDeploy) doLoadBalancer(e agent.Event) error {
 		switch p.Destination.Protocol {
 		case "HTTPS":
 			serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-backend-protocol"] = "http"
+			serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-proxy-protocol"] = "*"
+			realProto = "TCP"
+		case "SSL":
+			serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-backend-protocol"] = "tcp"
 			realProto = "TCP"
 		case "HTTP":
 			serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-backend-protocol"] = "http"
+			serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-proxy-protocol"] = "*"
 			realProto = "TCP"
 		case "TCP":
 			serviceAnnotations["service.beta.kubernetes.io/aws-load-balancer-backend-protocol"] = "tcp"
