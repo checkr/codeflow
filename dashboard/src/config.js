05720bbc61fd2e3f37f27a51dafee093742db8ed
Saso Matejina <sasso@users.noreply.github.com>
2017-04-14 14:48:07 -0700

    Use location.hostname in dev (#83)

--- dashboard/src/config.js
+++ dashboard/src/config.js
@@ -7,10 +7,10 @@ const loadConfig = () => {
 
   if (process.env.NODE_ENV === 'development') {
     config = {
-      "REACT_APP_ROOT": "http://localhost:"+process.env.REACT_APP_ROOT_PORT,
-      "REACT_APP_API_ROOT": "http://localhost:"+process.env.REACT_APP_API_ROOT_PORT,
-      "REACT_APP_WS_ROOT": "ws://localhost:"+process.env.REACT_APP_WS_ROOT_PORT,
-      "REACT_APP_WEBHOOKS_ROOT": "http://localhost:"+process.env.REACT_APP_WEBHOOKS_ROOT_PORT,
+      "REACT_APP_ROOT": "http://"+location.hostname+":"+process.env.REACT_APP_ROOT_PORT,
+      "REACT_APP_API_ROOT": "http://"+location.hostname+":"+process.env.REACT_APP_API_ROOT_PORT,
+      "REACT_APP_WS_ROOT": "ws://"+location.hostname+":"+process.env.REACT_APP_WS_ROOT_PORT,
+      "REACT_APP_WEBHOOKS_ROOT": "http://"+location.hostname+":"+process.env.REACT_APP_WEBHOOKS_ROOT_PORT,
       "REACT_APP_OKTA_CLIENT_ID": process.env.REACT_APP_OKTA_CLIENT_ID,
       "REACT_APP_OKTA_URL": process.env.REACT_APP_OKTA_URL,
       "REACT_APP_OKTA_LOGO": process.env.REACT_APP_OKTA_LOGO,
