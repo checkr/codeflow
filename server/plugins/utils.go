3c218ab81bd04ef9ed17828894bc5e6d7f79a6ba
Saso Matejina <matejina@gmail.com>
2017-10-31 11:02:31 -0700

    vendor

---
+++ server/plugins/utils.go
@@ -0,0 +1,16 @@
+package plugins
+
+import "regexp"
+
+func GetRegexParams(regEx, url string) (paramsMap map[string]string) {
+	var compRegEx = regexp.MustCompile(regEx)
+	match := compRegEx.FindStringSubmatch(url)
+
+	paramsMap = make(map[string]string)
+	for i, name := range compRegEx.SubexpNames() {
+		if i > 0 && i <= len(match) {
+			paramsMap[name] = match[i]
+		}
+	}
+	return
+}
